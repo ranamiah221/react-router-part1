755f0cad52e392cdeebb03ade7ec23bdb0ef0f3e
ranamiah221 <ranarasul21@gmail.com>
2024-10-10 20:00:31 +0600

    added spinners

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,28 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
+import { Audio } from "react-loader-spinner";
 
 
 const Home = () => {
+    const navigation=useNavigation();
     return (
     <> 
         <Header></Header>
-        <Outlet></Outlet>
+        {
+            navigation.state==='loading'? <Audio
+            height="80"
+            width="80"
+            radius="9"
+            color="green"
+            ariaLabel="three-dots-loading"
+            wrapperStyle
+            wrapperClass
+          />
+          :
+          <Outlet></Outlet>
+        }
+   
         <Footer></Footer>
     </> 
     );
